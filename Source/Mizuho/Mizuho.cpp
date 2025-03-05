0113924ad1b4435843c078da17acc645184130c5
nicolasdelisee <nicolas.delisee3@gmail.com>
2025-03-05 22:19:45 +0100

    All files renamed

--- Source/Mizuho/Mizuho.cpp
+++ Source/Mizuho/Mizuho.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "TestProject.h"
+#include "Mizuho.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mizuho, "Mizuho" );
